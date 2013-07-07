aa74b6572e01fba6871323c01e90292d2f5347b3
Dan Mandelstein <dmandels@gmail.com>
2013-07-07 16:57:56 +0000

    web.js to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var indexHtml = fs.readFileSync("index.html", 'utf8'); 
+  response.send(indexHtml);
 });
 
 var port = process.env.PORT || 5000;
